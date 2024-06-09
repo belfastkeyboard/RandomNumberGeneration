a20a474d1a5dad232075657c23706db874bde856
riain <ryanftoal@gmail.com>
2024-06-09 23:27:40 +0100

    Made a few renames and improvements

--- rng.hpp
+++ rng.hpp
@@ -11,40 +11,40 @@
  * 
  */
 
-namespace random
+namespace rng
 {
     /**
      * 
-     * @brief   Get a random number between __min and __max.
+     * @brief   Get a rng number between __min and __max.
      * 
      */
     template <typename T>
-    inline T number(T __min, T __max)
+    inline T number(T _min, T _max)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         static_assert(std::is_arithmetic<T>::value, "must be an arithmetic type");
         if (std::is_integral<T>::value)
-        { std::uniform_int_distribution<int> dist(__min, __max); return static_cast<T>(dist(gen)); } else
-        { std::uniform_real_distribution<double> dist(__min, __max); return static_cast<T>(dist(gen)); }
+        { std::uniform_int_distribution<int> dist(_min, _max); return static_cast<T>(dist(gen)); } else
+        { std::uniform_real_distribution<double> dist(_min, _max); return static_cast<T>(dist(gen)); }
     }
     /**
      * 
-     * @brief   Get a random number from a normal distribution.
+     * @brief   Get a rng number from a normal distribution.
      *          __mean specifies the peak of the bell-curve.
      *          Optional standard deviation, default = 10.0.
      * 
      */
     template <typename T>
-    inline T weighted_number(T __mean, double __std_dev)
+    inline T weighted_number(T _mean, double _std_dev)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         static_assert(std::is_arithmetic<T>::value, "must be an arithmetic type");
-        std::normal_distribution<double> dist(__mean, __std_dev);
+        std::normal_distribution<double> dist(_mean, _std_dev);
         return static_cast<T>(dist(gen));
     }
     /**
      * 
-     * @brief   Get a random number from a normal distribution between __min and __max.
+     * @brief   Get a rng number from a normal distribution between __min and __max.
      *          __mean specifies the  peak of the bell-curve.
      *          Optional standard deviation, default = 10.0.
      * 
@@ -52,34 +52,34 @@ namespace random
      * 
      */
     template <typename T>
-    inline T weighted_number(T __min, T __max, double __mean, double __std_dev)
+    inline T weighted_number(T _min, T _max, double _mean, double _std_dev)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         static_assert(std::is_arithmetic<T>::value, "must be an arithmetic type");
-        std::normal_distribution<double> dist(__mean, __std_dev);
+        std::normal_distribution<double> dist(_mean, _std_dev);
         T v;
         do {
             v = static_cast<T>(dist(gen));
-        } while (v < __min || v > __max); // we use a do-while loop here to ensure that the number returned is within the range without clamping
+        } while (v < _min || v > _max); // we use a do-while loop here to ensure that the number returned is within the range without clamping
         return v;                         // clamping tends to cluster numbers artificially around the clamp
     }                                     // this is an imperfect work-around
     /**
      * 
-     * @brief   Roll a random number against a percentage.
+     * @brief   Roll a rng number against a percentage.
      *          __x is clamped to 0, 100 if outside this range.
      * 
      */
     template <typename T>
-    inline bool percentage(T __x)
+    inline bool percentage(T _x)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         static_assert(std::is_arithmetic<T>::value, "percentage must be an arithmetic type");
-        std::max(T(0), std::min(T(100), __x));
-        return number(0, 100) < __x;
+        std::max(T(0), std::min(T(100), _x));
+        return number(0, 100) < _x;
     }
     /**
      * 
-     * @brief   Generates a random 64-bit UUID.
+     * @brief   Generates a rng 64-bit UUID.
      * 
      */
     inline uint64_t UUID()
@@ -90,7 +90,7 @@ namespace random
     }
     /**
      * 
-     * @brief   Generates a random 128-bit UUID.
+     * @brief   Generates a rng 128-bit UUID.
      * 
      */
     inline __uint128_t UUID128()
@@ -103,47 +103,47 @@ namespace random
     }
     /**
      * 
-     * @brief   Get a random index from a vector.
+     * @brief   Get a rng index from a vector.
      * 
      */
     template <typename T>
-    inline size_t index(std::vector<T> __vec)
+    inline size_t index(std::vector<T> _vec)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-        std::uniform_int_distribution<int> dist(0, __vec.size() - 1);
+        std::uniform_int_distribution<int> dist(0, _vec.size() - 1);
         return static_cast<size_t>(dist(gen));
     }
     /**
      * 
-     * @brief   Get a random index from a vector, using a normal distribution.
+     * @brief   Get a rng index from a vector, using a normal distribution.
      *          Optional standard deviation, default = 10.0.
      * 
      * @warning This function can enter an infinite loop as a result of its parameters.
      * 
      */
     template <typename T>
-    inline size_t weighted_index(std::vector<T> __v, size_t __mean, size_t __std_dev)
+    inline size_t weighted_index(std::vector<T> _v, size_t _mean, size_t _std_dev)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-        std::normal_distribution<double> dist(__mean, __std_dev);
+        std::normal_distribution<double> dist((double)(_mean), (double)(_std_dev));
         size_t v;
         do {
             v = static_cast<size_t>(dist(gen));
-        } while (v < 0 || v > __v.size() - 1);
+        } while (v > _v.size() - 1);
         return  v;
     }
     /**
      * 
-     * @brief   Get a random index from a vector, where the vector is an list of indices.
+     * @brief   Get a rng index from a vector, where the vector is an list of indices.
      * 
      */
     template <typename T>
-    inline size_t weighted_index(std::vector<T> __vec)
+    inline size_t weighted_index(std::vector<T> _vec)
     {
         static std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         static_assert(std::is_integral<T>::value, "weights vector must be an integral type");
-        std::uniform_int_distribution<size_t> dist(0, __vec.size());
-        return static_cast<size_t>(__vec[dist(gen)]);
+        std::uniform_int_distribution<size_t> dist(0, _vec.size());
+        return static_cast<size_t>(_vec[dist(gen)]);
     }
     /**
      * 
